cfb0eb02dfd96ca137e09e9da18932b5b4b9a648
Nguyen <hai.b.nguyen@accenturefederal.com>
2019-10-17 10:52:31 -0500

    Add Unit test

--- src/test/java/com/afs/test/MockApplication.java
+++ src/test/java/com/afs/test/MockApplication.java
@@ -3,7 +3,7 @@ package com.afs.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class MockApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MockApplication.class, args);
